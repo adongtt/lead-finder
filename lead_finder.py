ba149d8d2679d638434633a66024bc5e95da1805
adongtt <240732222@qq.com>
2026-05-26 16:59:39 +0800

    add some except website

--- lead_finder.py
+++ lead_finder.py
@@ -180,8 +180,8 @@ def detect_country(domain: str, email: str = "") -> str:
 # Big-brand / platform domains to exclude (no real decision-maker emails)
 EXCLUDED_DOMAINS = {
     # E-commerce giants
-    "amazon.com", "amazon.co.uk", "amazon.de", "amazon.fr", "amazon.ca", "amazon.in",
-    "ebay.com", "ebay.co.uk", "ebay.de",
+    "espn.com", "amazon.co.uk", "amazon.de", "amazon.fr", "amazon.ca", "amazon.in",
+    "ebay.com", "ebay.co.uk", "ebay.de","nfl.com","rei.com",
     "walmart.com", "target.com", "bestbuy.com", "costco.com",
     "alibaba.com", "aliexpress.com", "taobao.com", "tmall.com", "jd.com",
     "etsy.com", "wayfair.com", "overstock.com", "newegg.com",
